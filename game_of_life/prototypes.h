1d18123dd25eac78e2f592f2aa2444f90bf7dafd
Evgenius2020 <spleen47@mail.ru>
2017-07-07 16:17:12 +0700

    (#1) Fixed prototypes

--- game_of_life/prototypes.h
+++ game_of_life/prototypes.h
@@ -1,6 +1,8 @@
-void compute_square(int* matrix, int* buf, int x, int y);
-void compute_matrix(int* matrix, int height, int width, int threads);
 int part_reduce(int* mass, int length);
 int parts_reduce(int* results, int length);
-int total_reduce(int* matrix, int height, int width, int threads);
-void life_run(int* matrix, int height, int width, int threads);
+int total_reduce(int* matrix, int height, int width);
+
+void compute_element(int* matrix, int* buf, int x, int y, int height, int width);
+void compute_matrix(int* matrix, int height, int width);
+
+void life_run(int* matrix, int height, int width);
