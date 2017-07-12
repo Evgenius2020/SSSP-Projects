85adf6569ee2b4da3febbb8630ebe09892df6c3f
Evgenius2020 <spleen47@mail.ru>
2017-07-12 15:43:21 +0700

    Cnanged structure

--- source/lifeRun/lifeRun.h
+++ source/lifeRun/lifeRun.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
-#include "matrixCompute\matrixCompute.h"
-#include "totalReduce\totalReduce.h"
+#include "matrixCompute.h"
+#include "totalReduce.h"
 
 int lifeRun(int steps, int* matrix, int lineSize);
