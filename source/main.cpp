0d903d1bdb07e4701981959152a0313136b5c07e
Evgenius2020 <spleen47@mail.ru>
2017-07-13 10:31:55 +0700

    (#4) Works

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,19 @@
 #include "lifeRun.h"
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
+	int commRank, commSize;	
+	double startwtime = 0.0;
+  	double endwtime; 		
 	MPI_Init(&argc, &argv);
-	int commRank, commSize;
 	MPI_Comm_rank(MPI_COMM_WORLD, &commRank);
 	MPI_Comm_size(MPI_COMM_WORLD, &commSize);
+	if (commRank == 0) 
+  	{ 
+    	startwtime = MPI_Wtime();
+  	}
 	int steps = atoi(argv[1]);
 	int lineSize = atoi(argv[2]);
 	int* data = (int*)malloc(sizeof(int) * lineSize * lineSize);
@@ -30,6 +39,11 @@ int main(int argc, char* argv[]) {
 		}
 		fclose(out);
 	}
+	if (commRank == 0)
+  	{   
+    	endwtime = MPI_Wtime();
+    	cout << (endwtime-startwtime)*1000 << endl;      
+  	}
 	free(data);
 	MPI_Finalize();
 	return 0;
